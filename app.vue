a60f32f2915d007d9f11198c28e2be7fcfddccf6
Henry Owens <henryowens@Henrys-MacBook-Pro.local>
2024-05-12 18:40:50 +0200

    Adding google site verification

--- app.vue
+++ app.vue
@@ -8,6 +8,11 @@ useHead({
       content:
         "A simple site to help you collect coins from the European Union.",
     },
+    {
+      hid: "google-site-verification",
+      name: "google-site-verification",
+      content: "dtf3kB19TG-gqTReca_GLtIYMOHARUqc-LjIHaA22Qk",
+    },
   ],
   htmlAttrs: {
     lang: "en",
