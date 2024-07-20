7ccdfef2a427fd1f8e284bcc4f714ad598b1c7be
Henry Owens <henryowens@Henrys-MacBook-Pro.local>
2024-07-20 18:15:50 +0200

    Adding possibility to collapse countries

--- components/Country.vue
+++ components/Country.vue
@@ -8,35 +8,97 @@ defineEmits<{
 }>();
 
 provide("country", props);
+
+const isCollapsed = ref(false);
 </script>
 
 <template>
-  <div class="country">
-    <h2>{{ name }} {{ flag }}</h2>
-    <div class="country--coin__sets__container">
-      <CoinSet
-        v-for="(set, index) in coinSet"
-        :key="index"
-        v-bind="set"
-        @click="$emit('coin-clicked', { coin: $event, set })"
-      />
+  <div
+    class="country"
+    :class="{
+      collapsed: isCollapsed,
+    }"
+  >
+    <div class="country--title">
+      <h2>{{ name }} {{ flag }}</h2>
+
+      <span @click="isCollapsed = !isCollapsed">
+        <Transition
+          name="fade"
+          mode="out-in"
+          :delay="100"
+        >
+          <Icon
+            v-if="isCollapsed"
+            key="chevron-down"
+            name="ion:chevron-down-outline"
+          />
+          <Icon
+            v-else
+            key="chevron-up"
+            name="ion:chevron-up-outline"
+          />
+        </Transition>
+      </span>
     </div>
+
+    <Transition name="grow">
+      <div
+        v-if="!isCollapsed"
+        class="country--coin__sets__container"
+      >
+        <CoinSet
+          v-for="(set, index) in coinSet"
+          :key="index"
+          v-bind="set"
+          @click="$emit('coin-clicked', { coin: $event, set })"
+        />
+      </div>
+    </Transition>
   </div>
 </template>
 
 <style scoped lang="scss">
 .country {
   @apply p-6;
+  height: auto;
+  overflow: hidden;
+  &--title {
+    @apply flex justify-between items-center;
+    h2 {
+      @apply text-xl;
+      @apply text-gray-800;
+    }
 
-  h2 {
-    @apply text-xl;
-    @apply text-gray-800;
-    @apply mb-4;
+    svg {
+      @apply cursor-pointer;
+      @apply text-gray-500;
+    }
   }
 
   &--coin__sets__container {
+    @apply mt-4;
     @apply flex flex-col;
     @apply gap-6;
   }
+}
+
+.grow-enter-active {
+  transition: all 0.5s ease-in;
+  max-height: 0;
+}
+.grow-leave-active {
+  transition: all 0.5s ease-out;
+  max-height: 400px;
+}
+
+.grow-enter,
+.grow-leave-to {
+  max-height: 0;
+}
+
+.grow-enter-to,
+.grow-leave {
+  max-height: 400px;
 }
 </style>
