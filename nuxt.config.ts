0b29464f2df412e14bc1c42dc8da253ea81802fa
Henry Owens <henryowens@Henrys-MacBook-Pro-2.local>
2025-02-13 19:55:40 +0100

    Stab in the dark

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ["~/assets/styles/index.scss"],
   components: ["~/components", "~/components/core"],
+
   modules: [
     "@nuxtjs/eslint-module",
     "nuxt-icon",
@@ -17,29 +18,35 @@ export default defineNuxtConfig({
     "nuxt-lodash",
     "@vueuse/nuxt",
   ],
+
   nitro: {
-    baseURL: "http://localhost:8000",
     prerender: {
       crawlLinks: true,
       failOnError: false,
     },
   },
+
   ssr: true,
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
   runtimeConfig: {
     public: {
       env: process.env.NUXT_PUBLIC_ENV,
     },
   },
+
   app: {
     head: {
       charset: "utf-8",
       viewport: "width=device-width, initial-scale=1",
     },
   },
+
+  compatibilityDate: "2025-02-13",
 });
