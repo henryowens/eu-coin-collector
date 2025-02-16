31e654cbb2973ab3cc881b6e74656c3afb94f39d
Henry Owens <henryowens@Henrys-MacBook-Pro-2.local>
2025-02-17 00:58:46 +0100

    Adding loading states

--- pages/countries/[country_slug]/[coin_id].vue
+++ pages/countries/[country_slug]/[coin_id].vue
@@ -9,7 +9,7 @@ const route = useRoute();
 
 const coinId = computed(() => route.params.coin_id?.toString());
 
-const { data: coin } = useQuery({
+const { data: coin, isLoading: isCoinLoading } = useQuery({
   ...queries.countries.byCoin(coinId.value ?? ""),
   enabled: !!coinId.value,
 });
@@ -44,104 +44,163 @@ const onCoinSelect = async () => {
 
   await selectCoin(coinId.value);
 };
+
+const showSkeleton = computed(() => !coin.value || isCoinLoading.value);
 </script>
 
 <template>
   <div class="max-w-2xl mx-auto p-4 sm:py-8">
     <AuthDialog v-model="isAuthDialogOpen" />
-    <div v-if="coin">
-      <Breadcrumb class="mb-4 sm:mb-6">
-        <BreadcrumbList>
-          <BreadcrumbSeparator>
-            <Slash />
-          </BreadcrumbSeparator>
-          <BreadcrumbItem>
-            <BreadcrumbLink
-              :to="`/countries/${coin.coin_set_id.country_id.normalised_name}`"
-            >
-              {{ coin.coin_set_id.country_id.name }}
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbSeparator v-if="setTitle">
-            <Slash />
-          </BreadcrumbSeparator>
-          <BreadcrumbItem v-if="setTitle">
-            <BreadcrumbLink class="hover:text-muted-foreground">
-              {{ setTitle }}
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbSeparator>
-            <Slash />
-          </BreadcrumbSeparator>
-          <BreadcrumbItem>
-            <BreadcrumbLink
-              :to="`/countries/${country?.normalised_name}/${coin.id}`"
-            >
-              {{ coin.base_coin_id.name }}
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-        </BreadcrumbList>
-      </Breadcrumb>
+    <div>
+      <TransitionFadeBetween>
+        <Skeleton
+          v-if="showSkeleton"
+          class="w-[155px] h-5 mb-4 sm:mb-6"
+        />
+        <Breadcrumb
+          v-else
+          class="mb-4 sm:mb-6"
+        >
+          <BreadcrumbList>
+            <BreadcrumbSeparator>
+              <Slash />
+            </BreadcrumbSeparator>
+            <BreadcrumbItem>
+              <BreadcrumbLink
+                :to="`/countries/${coin?.coin_set_id.country_id.normalised_name}`"
+              >
+                {{ coin?.coin_set_id.country_id.name }}
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator v-if="setTitle">
+              <Slash />
+            </BreadcrumbSeparator>
+            <BreadcrumbItem v-if="setTitle">
+              <BreadcrumbLink class="hover:text-muted-foreground">
+                {{ setTitle }}
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator>
+              <Slash />
+            </BreadcrumbSeparator>
+            <BreadcrumbItem>
+              <BreadcrumbLink
+                :to="`/countries/${country?.normalised_name}/${coin?.id}`"
+              >
+                {{ coin?.base_coin_id.name }}
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </TransitionFadeBetween>
 
       <Card class="w-full max-w-[400px] mx-auto">
         <CardHeader class="flex flex-row justify-between gap-6">
           <div>
-            <img :src="imageUrl" />
+            <TransitionFadeBetween>
+              <Skeleton
+                v-if="showSkeleton"
+                class="w-[82px] h-[82px] rounded-full"
+              />
+              <img
+                v-else
+                :src="imageUrl"
+              />
+            </TransitionFadeBetween>
           </div>
           <div class="flex-1">
-            <CardTitle class="text-2xl font-bold tracking-wide mb-2">
-              {{ coin.base_coin_id.name }} Coin
-            </CardTitle>
+            <TransitionFadeBetween>
+              <Skeleton
+                v-if="showSkeleton"
+                class="w-[145px] h-[32px] mb-2"
+              />
+              <CardTitle
+                v-else
+                class="text-2xl font-bold tracking-wide mb-2"
+              >
+                {{ coin?.base_coin_id.name }} Coin
+              </CardTitle>
+            </TransitionFadeBetween>
             <CardDescription
-              v-if="country"
               class="text-sm text-muted-foreground flex flex-col gap-1"
             >
-              <span class="flex gap-2">
-                <Icon
-                  :name="`flag:${country?.locale}-1x1`"
-                  class="w-[20px] h-[20px] rounded-full"
-                />
-                {{ country?.name }}
-              </span>
+              <TransitionFadeBetween>
+                <div
+                  v-if="showSkeleton"
+                  class="flex gap-2 items-center"
+                >
+                  <Skeleton class="w-[22px] h-[22px] rounded-full" />
+                  <Skeleton class="w-[50px] h-[17px]" />
+                </div>
+                <span
+                  v-else
+                  class="flex gap-2 items-center"
+                >
+                  <Icon
+                    :name="`flag:${country?.locale}-1x1`"
+                    class="w-[20px] h-[20px] rounded-full"
+                  />
+                  {{ country?.name }}
+                </span>
+              </TransitionFadeBetween>
               <span v-if="setTitle">Part of the "{{ setTitle }}" set</span>
             </CardDescription>
           </div>
         </CardHeader>
 
         <CardFooter class="flex flex-col gap-2 px-6 pb-6">
-          <Button
-            v-if="!user || !selectedCoin"
-            class="w-full flex items-center justify-center gap-2"
-            size="lg"
-            @click="onCoinSelect"
-          >
-            Add To Collection
-            <Icon name="ion:plus" />
-          </Button>
-
-          <Button
-            v-else
-            class="w-full flex items-center justify-center gap-2"
-            size="lg"
-            variant="destructive"
-            @click="onCoinSelect"
-          >
-            Remove From Collection
-            <Icon name="ion:minus" />
-          </Button>
-          <NuxtLink
-            :to="`/countries/${country?.normalised_name}`"
-            class="w-full"
-          >
-            <Button
-              class="w-full flex items-center justify-center gap-2 text-accent-foreground"
-              variant="outline"
-              size="lg"
+          <TransitionFadeBetween>
+            <Skeleton
+              v-if="showSkeleton"
+              class="w-full h-[40px] rounded-xl"
+            />
+            <span
+              v-else
+              class="w-full"
             >
-              View Country
-              <Icon name="ion:eye" />
-            </Button>
-          </NuxtLink>
+              <Button
+                v-if="!user || !selectedCoin"
+                class="w-full flex items-center justify-center gap-2"
+                size="lg"
+                @click="onCoinSelect"
+              >
+                Add To Collection
+                <Icon name="ion:plus" />
+              </Button>
+
+              <Button
+                v-else
+                class="w-full flex items-center justify-center gap-2"
+                size="lg"
+                variant="destructive"
+                @click="onCoinSelect"
+              >
+                Remove From Collection
+                <Icon name="ion:minus" />
+              </Button>
+            </span>
+          </TransitionFadeBetween>
+
+          <TransitionFadeBetween>
+            <Skeleton
+              v-if="showSkeleton"
+              class="w-full h-[40px] rounded-xl"
+            />
+            <NuxtLink
+              v-else
+              :to="`/countries/${country?.normalised_name}`"
+              class="w-full"
+            >
+              <Button
+                class="w-full flex items-center justify-center gap-2 text-accent-foreground"
+                variant="outline"
+                size="lg"
+              >
+                View Country
+                <Icon name="ion:eye" />
+              </Button>
+            </NuxtLink>
+          </TransitionFadeBetween>
         </CardFooter>
       </Card>
     </div>
